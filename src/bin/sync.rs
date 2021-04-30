3fc0a6f921d474a09f1d821b315bc1ac7c851d85
suptalentdev <suptalentdev@gmail.com>
2021-04-30 13:24:04 +0300

    Fixup last release date

--- src/bin/sync.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::Result;
-
-use electrs::{Config, Daemon, Tracker};
-
-fn main() -> Result<()> {
-    let config = Config::from_args();
-    let daemon = Daemon::connect(&config)?;
-    Tracker::new(&config)?.sync(&daemon)
-}
