467979acdf7f80ff6db539b155d979fa615453b0
suptalentdev <suptalentdev@gmail.com>
2022-01-10 10:42:19 +0100

    Clearly state that altcoins are not supported.

--- src/daemon.rs
+++ src/daemon.rs
@@ -26,7 +26,7 @@ enum PollResult {
 }
 
 fn rpc_poll(client: &mut Client) -> PollResult {
-    match client.get_blockchain_info() {
+    match client.call::<BlockchainInfo>("getblockchaininfo", &[]) {
         Ok(info) => {
             let left_blocks = info.headers - info.blocks;
             if info.initial_block_download || left_blocks > 0 {
@@ -98,6 +98,20 @@ pub struct Daemon {
     rpc: Client,
 }
 
+// A workaround for https://github.com/rust-bitcoin/rust-bitcoincore-rpc/pull/190.
+#[derive(Deserialize)]
+struct BlockchainInfo {
+    /// The current number of blocks processed in the server
+    pub blocks: u64,
+    /// The current number of headers we have validated
+    pub headers: u64,
+    /// Estimate of whether this node is in Initial Block Download mode
+    #[serde(rename = "initialblockdownload")]
+    pub initial_block_download: bool,
+    /// If the blocks are subject to pruning
+    pub pruned: bool,
+}
+
 impl Daemon {
     pub(crate) fn connect(
         config: &Config,
@@ -128,8 +142,8 @@ impl Daemon {
         if !network_info.network_active {
             bail!("electrs requires active bitcoind p2p network");
         }
-        let info = rpc.get_blockchain_info()?;
-        if info.pruned {
+        let blockchain_info: BlockchainInfo = rpc.call("getblockchaininfo", &[])?;
+        if blockchain_info.pruned {
             bail!("electrs requires non-pruned bitcoind node");
         }
 
